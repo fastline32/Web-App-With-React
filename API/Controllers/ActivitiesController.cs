8175e4980be1f403bc1e4aeaeb36999a8b94c28a
Svetoslav <speednet@mail.bg>
2022-05-25 19:11:07 +0300

    InitialCreate

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _db;
+        public ActivitiesController(DataContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _db.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity (Guid id)
+        {
+            return await _db.Activities.FindAsync(id);
+        }
+    }
+}
